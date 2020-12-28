b58407479a124a2d7acbed418fada21a84a66527
jjenkins120 <jjenkins120@gmail.com>
2020-12-28 16:48:57 -0600

    cant decide styling approach

--- src/App.js
+++ src/App.js
@@ -8,96 +8,96 @@ import styled from 'styled-components'
 import image from './image.jpg'
 
 
-const App = () => {
-  return (
-    <Grid divided='vertically'>
-      <Grid.Row>
-        <Contact/>
-      </Grid.Row>
-      <Grid.Row>
-        <Image src={image} rounded style={styles.image}/>
-        <Bio/>
-      </Grid.Row>
-      <Grid.Row>
-        <ProjectsContainer/>
-      </Grid.Row>
-      <Grid.Row>
-        <Resume/>
-      </Grid.Row>
-    </Grid> 
-  )
-}
-
-const styles = {
-  grid:{
-
-  },
-  contactRow: {
-
-  },
-  image: { 
-
-  },
-  bioRow: {
-    display: 'flex'
-  },
-  projectsRow: {
-
-  }, 
-  resumeRow: {
-
-  }
-}
-
-
-
 // const App = () => {
 //   return (
-//         <ContainerStyle>
-//           <ContactStyle>
-//             <Contact/>
-//           </ContactStyle>
-//           <BioStyle>
-//             <Bio/>
-//           </BioStyle>  
-//           <ProjectsStyle>  
-//             <ProjectsContainer/>
-//           </ProjectsStyle>
-//           <ResumeStyle>
-//             <Resume/>
-//           </ResumeStyle>
-//         </ContainerStyle>
+//     <Grid divided='vertically'>
+//       <Grid.Row>
+//         <Contact/>
+//       </Grid.Row>
+//       <Grid.Row>
+//         <Image src={image} rounded style={styles.image}/>
+//         <Bio/>
+//       </Grid.Row>
+//       <Grid.Row>
+//         <ProjectsContainer/>
+//       </Grid.Row>
+//       <Grid.Row>
+//         <Resume/>
+//       </Grid.Row>
+//     </Grid> 
 //   )
 // }
 
-// const ContainerStyle = styled.div`
-//   flex-direction: column;
-//   height: 1500px;
-// `
+// const styles = {
+//   grid:{
 
-// const ContactStyle = styled.div`
-//   text-align: center;
-//   background-color: lime;
-//   height: 375px;
-// `
+//   },
+//   contactRow: {
 
-// const BioStyle = styled.div`
-//   background-image: url("${Image}");
-//   background-size: 800px; 
-//   text-align: center;
-//   height: 500px;
-// `
+//   },
+//   image: { 
 
-// const ProjectsStyle = styled.div`
-//   text-align: center;
-//   background-color: purple;
-//   height: 375px;
-// `
+//   },
+//   bioRow: {
+//     display: 'flex'
+//   },
+//   projectsRow: {
 
-// const ResumeStyle = styled.div`
-//   text-align: center;
-//   background-color: yellow;
-//   height: 375px;
-// `
+//   }, 
+//   resumeRow: {
+
+//   }
+// }
+
+
+
+const App = () => {
+  return (
+        <ContainerStyle>
+          <ContactStyle>
+            <Contact/>
+          </ContactStyle>
+          <BioStyle>
+            <Bio/>
+          </BioStyle>  
+          <ProjectsStyle>  
+            <ProjectsContainer/>
+          </ProjectsStyle>
+          <ResumeStyle>
+            <Resume/>
+          </ResumeStyle>
+        </ContainerStyle>
+  )
+}
+
+const ContainerStyle = styled.div`
+  flex-direction: column;
+  height: 1500px;
+`
+
+const ContactStyle = styled.div`
+  text-align: center;
+  background-color: lime;
+  height: 375px;
+`
+
+const BioStyle = styled.div`
+  background-image: url("${image}");
+  background-size: cover; 
+  text-align: center;
+  height: 1000px;
+`
+
+const ProjectsStyle = styled.div`
+  text-align: center;
+  background-color: purple;
+  height: 375px;
+`
+
+const ResumeStyle = styled.div`
+  text-align: center;
+  background-color: yellow;
+  height: 375px;
+`
 
 export default App
