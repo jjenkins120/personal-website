e944124667971280a542fcfe889120a40a48e703
jjenkins120 <jjenkins120@gmail.com>
2020-12-28 12:57:59 -0600

    basic setup

---
+++ src/components/Bio.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+const Bio = () => {
+  return (
+    <>
+      Bio
+    </>
+  )
+}
+
+export default Bio
---
+++ src/containers/Substance.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Bio from '../components/Bio'
+import Projects from '../components/Projects'
+import Resume from '../components/Resume'
+
+const Substance = () => {
+  return (
+    <>
+      <Bio/>
+      <Projects/>
+      <Resume/>
+    </>
+  )
+}
+
+export default Substance
