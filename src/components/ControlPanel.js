6ec6124fe978fdff7d556fc799cb4cd1b25ed808
jjenkins120 <jjenkins120@gmail.com>
2021-01-14 17:59:32 -0600

    restyled buttons

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ControlPanel = ({ displayPages }) => {
+const ControlPanel = ({ displayPages, file }) => {
     return (
         <>
-        {displayPages()}
+        {displayPages(file)}
         </>
     )
 }
