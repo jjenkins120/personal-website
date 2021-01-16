f5f0575b6aeb7cda3962df45729ee579abe6c13f
jjenkins120 <jjenkins120@gmail.com>
2021-01-16 14:29:15 -0600

    updated player

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,12 +4,20 @@ import ProjectTitle from '../components/ProjectTitle'
 
 const Project = ({ videoUrl, title, gitHubUrl }) => {
 
+  
   return (
     <div className='project'>
-      <ReactPlayer url={videoUrl} controls={true} width={'100%'} height={'45vw'} style={{ marginBottom: '5px', boxShadow: '0 30px 40px 0 rgba(16, 36, 94, 0.30)', border: 'solid 3px', borderRadius: '10px' }}/>
+      <ReactPlayer url={videoUrl} controls={true} width={'100%'} height={'45vw'} style={playerStyle}/>
       <ProjectTitle text={title} linkUrl={gitHubUrl}/> 
     </div>
   )
+}
+
+const playerStyle = { 
+  marginBottom: '5px', 
+  boxShadow: '0 30px 40px 0 rgba(16, 36, 94, 0.30)', 
+  border: 'solid 5px white', 
+  borderRadius: '5px' 
 }
 
 export default Project
